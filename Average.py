cce04a31c4da710a4cde7c66eec27b2a3cdb4984
swastika2015 <jmswastika18409@gmail.com>
2026-05-16 13:23:44 +0530

    Add files via upload

---
+++ Average.py
@@ -0,0 +1,13 @@
+tree1=98
+tree2=94
+tree3=41
+tree4=57
+tree5=36
+
+sum=tree1+tree2+tree3+tree4+tree5
+print("sum of all number is:", sum)
+
+#finding the average of a number
+
+Average = sum/5
+print("Average of total value is:", Average)
